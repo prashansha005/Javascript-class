840905b513c38dbc36e3806e0d9d14723e2ebfb4
Prashansha Neupane <neupane.prashansha2005@gmail.com>
2025-12-24 13:34:14 +0545

    operators

---
+++ Day7/script.js
@@ -0,0 +1,72 @@
+// let myTemp=Number(prompt("Enter your number in celcius"));
+// let myFahrenhite=(myTemp*9/5)+32;
+// let myKelvin=myTemp+273.15;
+// console.log(myFahrenhite);
+// console.log(myKelvin);
+
+// operators
+
+// arthemetic operator
+// logical opertors
+// type of operator
+// relational opertor
+// ternary operator
+// bitwise operator
+
+// ..............arthemetic operator............
+
+// 3+2 3 and 2 is operators and + is operators
+
+// mathematical operators
+// +,-,/,*,%
+
+console.log(20-20);
+console.log(20-20);
+console.log(20/2);
+console.log(10*2);
+ console.log(3%2);
+//   % or modulus remainder
+
+// .............logical operators........
+
+
+
+console.log(0+1 && false);  //false
+console.log(0-1 && true);  //true
+console.log(" abc" ||" " ||true); //abc
+console.log(true && " "&& ""); //blank
+console.log(""||"");  //blank
+console.log(true || false && true && "abc"); //true
+console.log(null || undefined); //undefined
+console.log(undefined || false);  //false
+console.log(null && true); //null
+console.log(null||true||false); //true
+
+// ...........Relational operator / comparision.........
+// < , > , <=, >= !=,==, === output always (true / false)
+console.log('10'==10); //true performs type coertion
+console.log('10'===10); //false doesnot perform type coertion
+
+let myNumber=Number(prompt("enter a number"));
+let Compare=(myNumber<=20);
+console.log(Compare);
+
+//kilometer to feet
+let myValue=Number(prompt('Enter your kilometers number'));
+console.log(myValue*3280.8);
+
+//kilometer to meters
+console.log(myValue*1000);
+
+//kilometer to inches
+console.log(myValue*39370);
+
+//kilometer to cm
+console.log(myValue*100000);
+
+//kilometer to yards
+console.log(myValue*1093.6);
+
+//kilometer to miles
+console.log(myValue*0.62137);
+
