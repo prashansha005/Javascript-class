95e9c82ac47b4c7fdea97868aaf2003d9f017c05
Prashansha Neupane <neupane.prashansha2005@gmail.com>
2025-12-24 13:32:16 +0545

    String intrepolation

---
+++ Day6/script.js
@@ -0,0 +1,116 @@
+// // STRING INTREPOLATION
+                                      
+// let myNumber=('shyam said "if there is one thing i hate its people who smoke!"');
+// console.log(myNumber);
+
+// let myPoem=`jonny jonny yes papa
+// Eating sugar no papa
+// Telling line no papa
+// open your mouth hahaha!!`;
+// console.log(myPoem);
+// // use BACKTICK(`)
+
+// let myFirstname= prompt('enter your first name');
+// let myLastname= prompt("enter your last name");
+// let myEmail= prompt("enter your email");
+// console.log(`my name is ${myFirstname} ${myLastname}.My Email is ${myEmail}`);
+// concratenation(+),string interpolation ($)
+
+// BMI CALCULATOR(BODY MASS INDEX)
+// let myHeight=prompt('Enter your height in feet');
+// let heightMeter= 0.3048 *myHeight; //convet feet to meter
+
+// let myWeight=prompt('Enter your weight in kg');
+// let actualHeight = heightMeter*heightMeter;
+// let myBMI=(myWeight/actualHeight)
+// console.log(`Your BMI is: ${myBMI}`);
+
+// let myMoney=prompt("Enter your nepali currency(NPR):");
+// let CNY= 20;
+// let BTN= 1.60;
+// let BDT= 1.16;
+// let INR= 1.60;
+// let MMK=0.064;
+// let LKR=0.36;
+// let JPY= 0.92;
+// let KRW=0.10;
+// console.log(`According to today's ${myMoney} is equal to ${myMoney/CNY} CNY`);
+// console.log(myMoney/CNY);
+// console.log(`According to today;s ${myMoney} is equal to ${myMoney/BTN} BTN`);
+// console.log(myMoney/BTN);
+// console.log(`According to today;s ${myMoney} is equal to ${myMoney/BDT} BDT`);
+// console.log(myMoney/BDT);
+// console.log(`According to today;s ${myMoney} is equal to ${myMoney/INR} INR`);
+// console.log(myMoney/INR);
+// console.log(`According to today;s ${myMoney} is equal to ${myMoney/MMK} MMK`);
+// console.log(myMoney/MMK);
+// console.log(`According to today;s ${myMoney} is equal to ${myMoney/LKR} LKR`);
+// console.log(myMoney/LKR);
+// console.log(`According to today;s ${myMoney} is equal to ${myMoney/JPY} JPY`);
+// console.log(myMoney/JPY);
+// console.log(`According to today;s ${myMoney} is equal to ${myMoney/KRW} KRW`);
+// console.log(myMoney/KRW);
+
+// ASSSIGMENT
+// let myMoney=prompt("Enter your nepali currency(NPR):");
+// let USD=132.50;
+// let EUR=142.00;
+// let GBP=165.00;
+// let AUD=88.00;
+// let CAD=97.00;
+// let CHF=149.00
+// let SGD=98.00;
+// let MYR=28.00;
+// let THB=3.65;
+// let AED=36.00;
+// let SAR=35.00;
+// let KWD=430.00;
+// let QAR=36.00;
+// let OMR=344.00;
+// let HKD=17.00;
+// let NZD=81.00;
+// let SEK=12.80;
+// let NOK=13.20;
+// let DKK=19.00;
+// let PHP=2.35;
+// console.log(`According to today's ${myMoney} is equal to ${myMoney/USD} USD`);
+// console.log((myMoney/USD).toFixed(2));
+// console.log(`According to today;s ${myMoney} is equal to ${myMoney/EUR} EUR`);
+// console.log((myMoney/EUR).toFixed(2));
+// console.log(`According to today;s ${myMoney} is equal to ${myMoney/GBP} GBP`);
+// console.log((myMoney/GBP).toFixed(2));
+// console.log(`According to today;s ${myMoney} is equal to ${myMoney/AUD} AUD`);
+// console.log((myMoney/AUD).toFixed(2));
+// console.log(`According to today;s ${myMoney} is equal to ${myMoney/CAD} CAD`);
+// console.log((myMoney/CAD).toFixed(2));
+// console.log(`According to today;s ${myMoney} is equal to ${myMoney/CHF} CHF`);
+// console.log((myMoney/CHF).toFixed(2));
+// console.log(`According to today;s ${myMoney} is equal to ${myMoney/SGD} SGD`);
+// console.log(myMoney/SGD);
+console.log(`According to today;s ${myMoney} is equal to ${myMoney/MYR} MYR`);
+console.log(myMoney/MYR);
+console.log(`According to today;s ${myMoney} is equal to ${myMoney/THB} THB`);
+console.log(myMoney/THB);
+console.log(`According to today;s ${myMoney} is equal to ${myMoney/AED} AED`);
+console.log(myMoney/AED);
+console.log(`According to today;s ${myMoney} is equal to ${myMoney/SAR} SAR`);
+console.log(myMoney/SAR);
+console.log(`According to today;s ${myMoney} is equal to ${myMoney/KWD} KWD`);
+console.log(myMoney/KWD);
+console.log(`According to today;s ${myMoney} is equal to ${myMoney/QAR} QAR`);
+console.log(myMoney/QAR);
+console.log(`According to today;s ${myMoney} is equal to ${myMoney/OMR} OMR`);
+console.log(myMoney/OMR);
+console.log(`According to today;s ${myMoney} is equal to ${myMoney/HKD} HKD`);
+console.log(myMoney/HKD);
+console.log(`According to today;s ${myMoney} is equal to ${myMoney/NZD} NZD`);
+console.log(myMoney/NZD);
+console.log(`According to today;s ${myMoney} is equal to ${myMoney/SEK} SEK`);
+console.log(myMoney/SEK);
+console.log(`According to today;s ${myMoney} is equal to ${myMoney/NOK} NOK`);
+console.log(myMoney/NOK);
+console.log(`According to today;s ${myMoney} is equal to ${myMoney/DKK} DKK`);
+console.log(myMoney/DKK);
+console.log(`According to today;s ${myMoney} is equal to ${myMoney/PHP} PHP`);
+console.log(myMoney/PHP);
+
