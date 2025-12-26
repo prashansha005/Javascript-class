48f37e8fcef30f7c1a316278f061aca34fa3e709
Prashansha Neupane <neupane.prashansha2005@gmail.com>
2025-12-26 21:42:42 +0545

    Do while

---
+++ Day13/script.js
@@ -0,0 +1,117 @@
+// let user=Number(prompt('enter a password'));
+// let attempt=6;
+// for(user=0;user<=6;user++);{
+//     if(user>1){
+//         console.log('')
+//     }
+
+// }
+
+// const ACTUAL_PIN=12345;
+// let NUMBER_OF_ATTEMPS=3;
+// for(;;){
+//     const userinput=Number(prompt('enter your pin'));
+//     if(NUMBER_OF_ATTEMPS && userinput==ACTUAL_PIN){
+//         console.log('you have been unlocked');
+//     }else{
+//         console.log('you have no attempt left or pin is incorrect');
+//         // NUMBER_OF_ATTEMPS--;
+//     }
+//     if(NUMBER_OF_ATTEMPS && userinput!=NUMBER_OF_ATTEMPS){
+//         console.log(`you have${NUMBER_OF_ATTEMPS--}`);
+//     }
+
+// }
+
+// let count=3;
+// while(count>0){
+//     console.log('hello world');
+//     count--;
+// }
+// let count=1;
+// while(count>0 && count<=10){
+//     console.log(`table of 5*${count} = ${5*count}`);
+//     count++;
+
+// }
+
+// let count=1;
+// do{
+//     console.log(`table of 5*${count}=${5*count}`);
+//     count++;
+// }while(count<=5);
+
+// let num=Number(prompt('enter a first number'));
+// let num2=Number(prompt('enter a second number'));
+// do{
+//     console.log(`num upto 100*${num2}=${100*num}`);
+//     num++;
+// }while(num>=num2 && num<num2);
+
+// let num1=Number(prompt('enter first number'));
+// let num2=
+
+// assigment
+//do while
+/* let A = Number(prompt("Enter the starting number:"));
+let B = Number(prompt("Enter the ending number:"));
+
+let sumEven = 0;
+let sumOdd = 0;
+
+let count = A;
+
+do {
+    if (count % 2 === 0) {
+        sumEven += count;
+    } else {
+        sumOdd += count;
+    }
+    count++;
+} while (count <= B);
+
+console.log(`Sum of even numbers between ${A} and ${B} is: ${sumEven}`);
+console.log(`Sum of odd numbers between ${A} and ${B} is: ${sumOdd}`);
+ */
+
+ 
+//While
+
+/* let A = Number(prompt("Enter the starting number:"));
+let B = Number(prompt("Enter the ending number:"));
+
+let sumEven = 0;
+let sumOdd = 0;
+
+let count = A;
+
+while (count <= B) {
+    if (count % 2 === 0) {
+        sumEven += count;
+    } else {
+        sumOdd += count;
+    }
+    count++;  
+}
+
+console.log(`Sum of even numbers between ${A} and ${B} is: ${sumEven}`);
+console.log(`Sum of odd numbers between ${A} and ${B} is: ${sumOdd}`); */
+
+// while(condition)
+// do while(statement)
+
+//  for(let count=1; count<=100; count++){
+//  console.log('count');
+//  }
+
+//  let A=prompt('enter first number');
+//  let B=prompt('enter second number'); 
+//  for(let A=1;A<=B;A=A+2){
+//     console.log(A);
+//  }
+
+let A=prompt('enter a number');
+let B=prompt('enter a number');
+for(let A=2;A<=B;A++);{
+    
+}
