1e42509f7b3a9e6a9d06d5853c93a4a7c8efa42f
Prashansha Neupane <neupane.prashansha2005@gmail.com>
2025-12-27 11:48:22 +0545

    array destruction

---
+++ Day17/script.js
@@ -0,0 +1,149 @@
+// let listofname=['abc','cde','efg'];
+// let popedvalue=listofname.pop();
+// console.log(listofname);
+// console.log(popedvalue);
+
+// let removedvalue=listofname.shift();
+// console.log(removedvalue);
+
+// listofname.unshift('added on first');
+// console.log(listofname);
+
+// listoffaculty=['science','management','arts','commerce','humanity'];
+// let popedvalue=listoffaculty();
+// console.log(popedvalue=listoffaculty());
+
+// let removedvalue=listoffaculty.shift();
+// console.log(removedvalue);
+
+// listoffaculty.unshift('added on first');
+// console.log(listoffaculty);
+
+// let myarray=[10,20,30,40,50,70,80,90,100,60];
+// console.log(myarray[myarray.length -1]);
+// console.log(myarray.indexOf(50));
+// console.log(myarray.lastIndexOf(50));
+
+// myarray[myarray.indexOf(60)]=16;
+// console.log(myarray);
+
+// let myarraychunk=myarray.slice(20,50); //(start,end -1),immutiable
+// console.log(myarraychunk);
+// console.log(myarray);
+
+// crossOriginIsolated.log(myarray.indexOf(100)); //-1
+
+// myarray.splice(0,3); //mutable
+// console.log(myarray);
+
+// ............ARRAY DESTRUCTION..........
+// let [first, , , fourth] =['ram','shyam','hari','sita'];
+// console.log(first);
+// console.log(fourth);
+
+// let [first,second,third, , , , ,eight,nine,ten]=['one','two','three','four','fifth','sixt','seven','seven','eight','nine','ten'];
+// console.log(first,second,third);
+// console.log(eight,nine,ten);
+
+// .........ARRAY SPREAD.........
+
+// let firstArray=[1,2,3,4,5];
+// let secondArray=[6,7,8,9,10];
+
+// let joinedArray=[...firstArray, ...secondArray];
+// console.log(joinedArray);
+
+// console.log(...firstArray, ...secondArray);
+
+// let[first, second, ...rest]=['ram','shyam','hari','sita']
+// console.log(...rest);
+
+
+// .......ARRAY.......  (COLLECTION OF ITEMS);
+
+// ARRAY METHODS
+
+// PUSH():add to end
+// POP():delete from end and return
+// SHIFT():add to start
+// UNSHIFT():delete from start and return
+// SLICE():returns a piece of array
+// SPLICE():change original array(add,remove,replace)
+
+// ARRAY DETRUCTION:extracts values from an array into variables.
+// ARRAY SPREAD:(...)expands an array elements.
+
+
+// Assigment
+// push
+// let listofstudent=['name','rollno','address']
+// console.log(listofstudent[0]);
+// console.log(listofstudent[1]);
+
+// listofstudent.push('name');
+// console.log(listofstudent[2]);
+
+// // let listofStudent=['name','rollno','address']
+// // console.log(listofStudent[0]);
+// // console.log(listofStudent[1]);
+
+// // listofStudent.push('name');
+// // console.log(listofStudent[2]);
+
+// // POP
+// let listofname=['riya','priya','reha'];
+// let popedvalue=listofname.pop();
+// console.log(listofname);
+// console.log(popedvalue);
+
+// // let listofname=['ram','shyam','hari'];
+// // let popedValue=listofname.pop();
+// // console.log(listofname);
+// // console.log(popedValue);
+
+// // SHIFT
+// let removedvalue=listofname.shift();
+// console.log(removedvalue);
+
+// // let removedValue=listofname.shift();
+// // console.log(removedValue);
+
+// // UNSHIFT
+// listofname.unshift('added on first');
+// console.log(listofname);
+
+// // listofname.unshift('added on first');
+// // console.log(listofname);
+
+// // SLICE
+// let myarraychunk=myarray.slice(20,50); //(start,end -1),immutiable
+// console.log(myarraychunk);
+// console.log(myarray);
+
+// // let myarraychunk=myarray.slice(20,50); //(start,end -1),immutiable
+// // console.log(myarraychunk);
+// // console.log(myarray);
+
+// // SPLICE
+// myarray.splice(0,3); 
+// console.log(myarray);
+
+// // myarray.splice(0,3); 
+// // console.log(myarray);
+
+// // ARRAY DETURCTION
+// let [first, , , fourth] =['ram','shyam','hari','sita'];
+// console.log(first);
+// console.log(fourth);
+
+// // let [first, , , fourth] =['ram','shyam','hari','sita'];
+// // console.log(first);
+// // console.log(fourth);
+
+// // ARRAY SPREAD 
+// let[First, second, ...rest]=['ram','shyam','hari','sita']
+// console.log(...rest);
+
+// // let[First, second, ...rest]=['ram','shyam','hari','sita']
+// // console.log(...rest);
+
