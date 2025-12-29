405bde41c7e8e8fe65b49091ea950451e079ae9e
Prashansha Neupane <neupane.prashansha2005@gmail.com>
2025-12-29 09:21:59 +0545

    creating a function

---
+++ Day25/script.js
@@ -0,0 +1,34 @@
+// .........create a function to print name.........
+
+// use parameter or take input from parameter
+
+// create a function to calculate area of rectangle
+// use parameters
+
+// create a function to print welcome message
+// output should be hello_name_,welcome to our platform.
+// its good to know you are from_address_
+// see you next_dayname_
+
+function printname(prashansha) {
+  console.log(printname);
+}
+function createrectangulararea(length, width) {
+  const area = length * width;
+  console.log("area of rectangle" + area);
+  return area;
+}
+
+function welcomeMessage(name, address, dayname) {
+  console.log(
+    `Hello ${name},welcome to our platform.\n its good to know you are from ${address} \n see you next ${dayname}`
+  );
+}
+welcomeMessage("prashansha", "butwal", "friday");
+
+function welcomemessage(name, address, dayname) {
+  console.log(
+    `Hello ${name},welcome to our platform.\n its good to know you are from ${address} \n see you next ${dayname}`
+  );
+}
+welcomemessage("prashansha", "butwal", "friday");
